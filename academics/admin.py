9808111c63ee205b67bde7f7216a28988b9a9727
Bimal Raj Gyawali <gyawali.rajbimal35@gmail.com>
2020-04-14 00:14:45 +0545

    models added in academics

---
+++ academics/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Program, Subject, Semester
+
+admin.site.register(Program)
+admin.site.register(Subject)
+admin.site.register(Semester)
---
+++ academics/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+class Program(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Subject(models.Model):
+    code = models.CharField(max_length=10)
+    name = models.CharField(max_length=100)
+    credit = models.IntegerField()
+    programs = models.ManyToManyField(Program)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Semester(models.Model):
+    sem = models.IntegerField(
+        validators=[
+            MaxValueValidator(8),
+            MinValueValidator(1)
+        ])
+
+    subjects = models.ManyToManyField(Subject)
+    program = models.ForeignKey(Program, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.program} - {self.sem} sem'
