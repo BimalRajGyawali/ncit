818891552a00b9d69d90f3bfec671b38a31563e4
Bimal Raj Gyawali <gyawali.rajbimal35@gmail.com>
2020-05-18 12:42:57 +0545

    registers models to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Student, StudentLogin
 
-# Register your models here.
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['roll', 'name', 'sem', 'phone', 'registered']
+
+class StudentLoginAdmin(admin.ModelAdmin):
+    list_display = ['roll', 'password', 'student']
+
+
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Student, StudentLoginAdmin)
