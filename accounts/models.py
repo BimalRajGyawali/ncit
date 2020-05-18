e8d1ce22b843afc1076549529a9cf466c3c72889
Bimal Raj Gyawali <gyawali.rajbimal35@gmail.com>
2020-05-18 12:42:38 +0545

    adds Student and StudentLogin models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    roll = models.IntegerField(unique=True)
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=10, unique=True)
+    sem = models.IntegerField()
+    registered = models.BooleanField(default=False)
+    email = models.EmailField(default=None)
+
+    def __str__(self):
+        return self.name
+
+
+
+class StudentLogin(models.Model):
+    roll = models.IntegerField(unique=True)
+    password = models.CharField(max_length=100)
+    student = models.OneToOneField(on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.roll
