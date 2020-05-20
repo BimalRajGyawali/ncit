2f8504bfec0d1f6a8b42bfa46db340fc47e6fa09
Bimal Raj Gyawali <gyawali.rajbimal35@gmail.com>
2020-05-20 16:02:05 +0545

    completes register validation

--- accounts/static/accounts/js/script.js
+++ accounts/static/accounts/js/script.js
@@ -6,13 +6,13 @@ var left, opacity, scale; //fieldset properties which we will animate
 var animating; //flag to prevent quick multi-click glitches
 
 $(".next").click(function(){
-
+/* Prevent empty fields */
 let inputs = document.getElementById(`fieldset${event.target.id}`).getElementsByTagName('input');
 
+let len = inputs.length - event.target.id;   //removing buttons from length
 
- for(let i=0; i<inputs.length; i++){
-
-    if(!inputs[i].value.trim()){
+ for(let i=0; i<len; i++){
+    if(!inputs[i].value){
             inputs[i].focus();
             inputs[i].style.outline = '1px solid red';
             inputs[i].classList.add('error');
@@ -26,6 +26,27 @@ let inputs = document.getElementById(`fieldset${event.target.id}`).getElementsBy
          return false;
      }
   }
+ if(event.target.id == 1){
+        /* Confirm roll is a number */
+let roll = inputs[0].value;
+ if(!Number.isInteger(parseInt(roll))){
+
+      inputs[0].value = '';
+      inputs[0].placeholder = `${roll} is not a valid roll number`;
+      inputs[0].focus();
+      inputs[0].style.outline = '1px solid red';
+      inputs[0].classList.add('error');
+      inputs[0].addEventListener('keydown', ()=>{
+        inputs[0].style.outline = 0;
+        inputs[0].style.color = 'black';
+        inputs[0].classList.remove('error');
+        inputs[0].placeholder = 'Enter your roll number eg: 181522';
+    })
+     return false;
+ }
+
+
+ }
 
 
 	if(animating) return false;
@@ -100,9 +121,10 @@ $(".previous").click(function(){
 $(".submit").click(function(){
 event.preventDefault();
 let inputs = document.getElementById(`fieldset${event.target.id}`).getElementsByTagName('input');
+let len = inputs.length - 2;   //removing buttons from length
 
- for(let i=0; i<inputs.length; i++){
-     if(!inputs[i].value.trim()){
+ for(let i=0; i<len; i++){
+     if(!inputs[i].value){
             inputs[i].focus();
             inputs[i].style.outline = '1px solid red';
             inputs[i].classList.add('error');
@@ -137,6 +159,42 @@ let inputs = document.getElementById(`fieldset${event.target.id}`).getElementsBy
      return false;
   }
 
+
+  let password = inputs[1].value;
+  let password1 = inputs[2].value;
+  let pswdError = document.getElementById('pswd-error');
+  let confirmPswdError = document.getElementById('confirm-pswd-error');
+
+
+  if(password.length < 5 || password.indexOf(' ')>=0){
+     pswdError.textContent = 'Password must be at least 5 characters long with no spaces';
+     inputs[1].focus();
+     inputs[1].style.outline = '1px solid red';
+
+  inputs[1].addEventListener('keydown', ()=>{
+    pswdError.textContent = '';
+    inputs[1].style.outline = '';
+    confirmPswdError.textContent = '';
+    inputs[2].style.outline = '';
+  })
+
+     return false;
+
+  }
+
+  /* check whether two pswds are same or not */
+  if(password !== password1){
+        confirmPswdError.textContent = "Passwords didn't match";
+        inputs[2].focus();
+        inputs[2].style.outline = '1px solid red';
+
+        inputs[2].addEventListener('keydown', ()=>{
+              confirmPswdError.textContent = '';
+              inputs[2].style.outline = '';
+        })
+
+    return false;
+  }
 
 
 
